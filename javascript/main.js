7d71981a4f11508dbd08c1941e4cbf9d2d312483
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-06 16:53:59 +0100

    square(n) sum

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,12 @@ function even_or_odd(number) {
     return number%2==0?"Even":"Odd"
 }
 */
+/*
+function squareSum(numbers){
+  let count=0;
+  for(let i = 0;i<numbers.length;i++){
+    count +=i ** 2
+  }
+  return count
+}
+*/
