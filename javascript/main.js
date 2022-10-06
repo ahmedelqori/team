04272146b5d0fbdc7d50e9d54dfd6381cd02fc7d
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-06 17:59:31 +0100

    find needle

--- javascript/main.js
+++ javascript/main.js
@@ -13,11 +13,20 @@ function squareSum(numbers){
 }
 */
 /*
-function lovefunc(flower1, flower2){
+function lovefun    c(flower1, flower2){
     if(flower1 %2==0&&flower2%2==1||flower2 %2==0&&flower1 %2 ==1){
       return true
     }else{
       return false
     }
   }
+*/
+/*
+function findNeedle(haystack) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] == "needle") {
+      return "found the needle at position " + i;
+    }
+  }
+}
 */
