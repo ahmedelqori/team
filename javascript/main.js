95f6c89230c063f2f36149f4427974d8f3c1ade2
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-07 11:13:20 +0100

    if you cant sleep, Just Count Sheep

--- javascript/main.js
+++ javascript/main.js
@@ -44,3 +44,12 @@ function findAverage(array) {
   }
 }
 */
+/*
+var countSheep = function (num){
+  let string = ""
+  for(let i =0;i<num;i++){
+    string+=(i+1)+" sheep..."
+  }
+  return string
+}
+*/
