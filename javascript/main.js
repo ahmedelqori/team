f0088793bfc62e2b31ca73623194912bf3086e52
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-10 19:20:15 +0100

    Isogram

--- javascript/main.js
+++ javascript/main.js
@@ -216,4 +216,17 @@ function isIsogram(str) {
 
   return array == str.toLowerCase() ? true : false;
 }
+
+function isIsogram(str) {
+  let array = str.toLowerCase().split("");
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] == array[j] && i != j) {
+        count = 1;
+      }
+    }
+  }
+  return count == 1 ? false : true;
+}
 */
