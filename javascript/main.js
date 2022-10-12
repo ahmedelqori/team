95d21cb59b6c17ef2a47347d70159f2420b3f3e0
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-12 12:08:04 +0100

    Simple Pig Latin

--- javascript/main.js
+++ javascript/main.js
@@ -430,4 +430,67 @@ var number = function(busStops){
   }
   return count
 }
+*/ /*
+function pickPeaks(arr) {
+  let a = [];
+  let b = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i] > arr[i - 1] &&
+      arr[i - 1] >= arr[i - 2] &&
+      arr[i] > arr[i + 1] &&
+      arr[i + 1] >= arr[i + 2]
+    ) {
+      b.push(arr[i]);
+      a.push(i);
+    } else if (
+      arr[i] > arr[i - 1] &&
+      arr[i] == arr[i + 1] &&
+      arr[i + 1] >= arr[i + 2]
+    ) {
+      b.push(arr[i]);
+      a.push(i);
+    } else if (
+      arr[i] > arr[i + 1] &&
+      arr[i] > arr[i - 1] &&
+      arr[i + 1] <= arr[i + 2] &&
+      arr[i - 1] <= arr[i - 2]
+    ) {
+      b.push(arr[i]);
+      a.push(i);
+    }
+  }
+  return { pos: a, peaks: b };
+}
+
+console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1])); // {pos:[3,7], peaks:[6,3]}
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3])); // {pos:[3,7], peaks:[6,3]}
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1])); // {pos:[3,7], peaks:[6,3]}
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1])); // {pos:[3,7], peaks:[6,3]}
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2])); // {pos:[3,7], peaks:[6,3]}
+console.log(
+  pickPeaks([
+    1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3,
+  ])
+); // {pos:[3,7], peaks:[6,3]}
+*/
+/*
+function pigIt(str) {
+  let array = str.split(" ");
+  let arr1 = array.map((ele) => ele.slice(1));
+  let arr2 = array.map(function (ele) {
+    if (ele != "!") {
+      return ele.charAt(0) + "ay";
+    } else {
+      return ele;
+    }
+  });
+  let result = [];
+
+  for (let i = 0; i < arr2.length; i++) {
+    result.push(arr1[i] + arr2[i]);
+  }
+  return result.join(" ");
+}
+console.log(pigIt("Pig latin is cool !"));
 */
