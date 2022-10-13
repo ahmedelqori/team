636e3c068b2273676eaf4ac46d7e635f39e1825d
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-13 17:07:00 +0100

    Highest Scoring Word

--- javascript/main.js
+++ javascript/main.js
@@ -624,3 +624,49 @@ function expandedForm(num) {
   return result.join(" + ");
 }
 */
+/*
+function high(x) {
+  let alphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+
+  let arr = x.split(" ");
+  let array = arr.map(function (ele) {
+    let count = 0;
+    for (let i = 0; i < ele.length; i++) {
+      count += alphabet[ele[i]];
+    }
+    return count;
+  });
+
+  return arr.slice(
+    array.indexOf(Math.max(...array)),
+    array.indexOf(Math.max(...array)) + 1
+  );
+}
+*/
