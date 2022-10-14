ee5e882fb49a5bb2acb7347f73d9627e9bf12e83
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2022-10-14 23:30:18 +0100

    Operators

--- javascript/elzeroTaklif.js
+++ javascript/elzeroTaklif.js
@@ -1,44 +1,63 @@
-/*
-function pickPeaks(arr) {
-  let a = [];
-  let b = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (
-      arr[i] > arr[i - 1] &&
-      arr[i - 1] >= arr[i - 2] &&
-      arr[i] > arr[i + 1] &&
-      arr[i + 1] >= arr[i + 2]
-    ) {
-      b.push(arr[i]);
-      a.push(i);
-    } else if (
-      arr[i] > arr[i - 1] &&
-      arr[i] == arr[i + 1] &&
-      arr[i + 1] >= arr[i + 2]
-    ) {
-      b.push(arr[i]);
-      a.push(i);
-    } else if (
-      arr[i] > arr[i + 1] &&
-      arr[i] > arr[i - 1] &&
-      arr[i + 1] <= arr[i + 2] &&
-      arr[i - 1] <= arr[i - 2]
-    ) {
-      b.push(arr[i]);
-      a.push(i);
-    }
-  }
-  return { pos: a, peaks: b };
-}
+/* ------------------ Operators ----------- */
 
-console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1])); // {pos:[3,7], peaks:[6,3]}
-console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3])); // {pos:[3,7], peaks:[6,3]}
-console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1])); // {pos:[3,7], peaks:[6,3]}
-console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1])); // {pos:[3,7], peaks:[6,3]}
-console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2])); // {pos:[3,7], peaks:[6,3]}
-console.log(
-  pickPeaks([
-    1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3,
-  ])
-); // {pos:[3,7], peaks:[6,3]}
+// $$$$$$$$$ --I-- $$$$$$$$$ //
+
+// console.log(10 * 20 * (15 % 3) * 190 * 10 * 400); // 0
+
+// $$$$$$$$$ --II-- $$$$$$$$$ //
+
+/*
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * --num); // 6
+
+// Soultion Three
+console.log((typeof num).length); // 6
+
+// Soultion Four
+console.log((typeof ("" + num)).length); // 6
+
+// Solution Five
+console.log((typeof !num).length - --num); // 6
+
+// Solution Six
+console.log(++num * num + num); // 6
+*/
+
+// $$$$$$$$$ --III-- $$$$$$$$$ //
+
+/*
+
+let num = "10";
+
+// Solution One
+console.log(+num + +num); // 20
+
+// Solution Two
+console.log(num.length * +num); // 20
+
+// Solution Three
+console.log((+num / num.length) * (num.length + num.length)); // 20
+
+// Solution Four
+console.log((+num / num.length) ** num.length - +num / num.length); // 20
+
+*/
+
+// $$$$$$$$$ --IV-- $$$$$$$$$ //
+
+/*
+let points = 10;
+let a = "" + points;
+points = ++points + a.length;
+
+console.log(points); // 13
+
+points = --points - a.length * a.length;
+
+console.log(points); // 8;
 */
